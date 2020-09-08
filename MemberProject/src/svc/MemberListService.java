efaa42a24c4444c774cffd1c968f8bf38bf20587
yn-s01 <yn-s01@DESKTOP-B037NE3>
2020-09-08 16:22:18 +0900

    0908

---
+++ MemberProject/src/svc/MemberListService.java
@@ -0,0 +1,20 @@
+package svc;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import dao.MemberDAO;
+import static db.JdbcUtil.*;
+import vo.MemberBean;
+
+public class MemberListService {
+	
+	public ArrayList<MemberBean> getMemberList() {
+		Connection con = getConnection();
+		MemberDAO memberDAO = MemberDAO.getInstance();
+		memberDAO.setConnection(con);
+		ArrayList<MemberBean> memberList = memberDAO.selectMemberList();
+		close(con);
+		return memberList;
+	}
+
+}
